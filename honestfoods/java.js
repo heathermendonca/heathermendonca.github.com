0b847d19fc723aa4d4437db53044d562a8038905
Heather <heathermendonca@Heathers-iMac.local>
2013-03-20 13:11:31 -0400

    ugh….. TT_TT

--- honestfoods/java.js
+++ honestfoods/java.js
@@ -5,10 +5,40 @@ alert('HEY GUUUURL ITS LOADING');
 
 //circle flipping attempt
 
-$(function() {
+/* $(function() {
   function flipCard() {
   	$(this).toggleClass("flip");
 }
   
-  $("#big-circle").on("click", flipCard);
+  $("#circle-holder").on("click", flipCard);
 });
+
+*/
+
+//scrollorama
+//v is that just for html?
+$(document).ready(function() {
+    var scrollorama = $.scrollorama({
+        blocks:'.scrollblock'
+    });
+});
+
+scrollorama.onBlockChange(function() {
+		var i = scrollorama.blockIndex;
+		$('#console')
+		.css('display','block')
+		.text('onBlockChange | blockIndex:'+i+' | current block: '+scrollorama.settings.blocks.eq(i).attr('id'));
+});
+
+
+//scrollorama target --- my stuff
+
+scrollorama
+	//.animate('#example1',{duration:400, property:'opacity'})
+	.animate('#textone',{ delay: 100, duration: 100, start:-1400, end:0 })
+
+
+
+//
+alert('D:<');
+//test test test guuurl
