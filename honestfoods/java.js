cb332cbdbb52787ed668149d9d327be5572fda2a
Heather Mendonca <heathermendonca@cloudkid.com>
2013-03-20 15:17:20 -0400

    working..

--- honestfoods/java.js
+++ honestfoods/java.js
@@ -28,8 +28,13 @@ $(document).ready(function() {
 	
 	scrollorama
 		//.animate('#example1',{duration:400, property:'opacity'})
-		.animate('#circle-holder',{ delay: 0, duration: 50, property: 'left', start:-1400, end:0 })
-		.animate('#textone',{ delay: 100, duration: 300, property: 'left', start:-1400, end:0 });
+		.animate('#circle-holder',{ delay: 0, duration: 50, property: 'rotate-y', start:-1400, end:0 })
+		.animate('#textone',{ delay: 100, duration: 300, property: 'left', start:-1400, end:0 })
+		.animate('#texttwo',{ delay: 150, duration: 300, property: 'left', start:-1400, end:0 })
+		.animate('#textthree',{ delay: 200, duration: 300, property: 'left', start:-1400, end:0 })
+		.animate('#textfour',{ delay: 250, duration: 300, property: 'left', start:-1400, end:0 })
+		.animate('#textfive',{ delay: 300, duration: 300, property: 'left', start:-1400, end:0 });
+
 });
 
 
